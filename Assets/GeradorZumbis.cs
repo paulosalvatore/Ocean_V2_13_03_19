e28329931fa89ede1c28d1fc25c8141b919a35ef
Paulo Salvatore <paulo@salvatore.tech>
2019-03-14 09:25:00 -0300

    Initial commit

---
+++ Assets/GeradorZumbis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorZumbis : MonoBehaviour {
+
+	public float delay = 2f;
+	public GameObject zumbiPrefab;
+
+	public float area = 3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("GerarZumbi", delay, delay);
+	}
+	
+	void GerarZumbi () {
+		GameObject zumbi = Instantiate(zumbiPrefab);
+		Vector2 posicaoAleatoria = Random.insideUnitCircle * area;
+		zumbi.transform.position = new Vector3(
+			posicaoAleatoria.x,
+			zumbiPrefab.transform.position.y,
+			posicaoAleatoria.y
+		);
+	}
+}
---
+++ Assets/Zumbi.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zumbi : MonoBehaviour {
+
+	public float velocidade = 0.35f;
+	Rigidbody rb;
+	Animator animator;
+	bool vivo = true;
+
+	GameObject jogador;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+
+		jogador = GameObject.Find("Jogador");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vivo)
+		{
+			transform.LookAt(jogador.transform);
+			transform.eulerAngles = new Vector3(
+				0,
+				transform.rotation.eulerAngles.y,
+				0
+			);
+
+			rb.velocity = transform.forward * velocidade;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Projétil"))
+		{
+			animator.SetTrigger("Die");
+			vivo = false;
+		}
+	}
+}
